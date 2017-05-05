c878f3784de14cc6146a4878b7389ccb39f85f00
EmilyDeng <doggydeng@gmail.com>
2017-05-04 21:26:31 -0400

    write a function to read local xlsx file

--- read_data.py
+++ read_data.py
@@ -2,18 +2,52 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import xlrd
+import sys
+import math
 
-rdn = []
-filename = '../data/input/random.dat'
-with open(filename) as infile:
-    for lines in infile:
-        if lines:
-            data = lines.strip('\n')
-            cell = data.split(',')
-            if len(cell) == 4:
-                rdncell = cell[2].split(':')
-                rdn.append(float(rdncell[1]))
+def read_xlsx(path):
+    workbook = xlrd.open_workbook(path)
+    second_sheet = workbook.sheet_by_index(1)
+    row_start = 3
+    koff0, sigma, steps, force, cate = [], [], [], [], []
+    n_rows = second_sheet.nrows
+    while row_start < n_rows:
+        cells = second_sheet.row_slice(row_start, start_colx = 0, end_colx = 7)
+        row_start += 1
+        koff0.append(cells[0].value)
+        sigma.append(cells[1].value)
+        steps.append(cells[-2].value)
+        if cells[-1].value > 0.5:
+            cate.append('h')
+        elif cells[-1].value > 0.2:
+            cate.append('m')
+        else:
+            cate.append('l')
+        force.append(cells[-1].value)
+    return koff0, sigma, steps, force, cate
 
-plt.hist(rdn)
-plt.show()
-fig = plt.gcf()
+def plot():
+    path = str(sys.argv[1])
+    koff0, sigma, steps, force, cate = read_xlsx(path)
+
+    fig1 = plt.figure()
+    plt.hist(steps)
+    plt.title('RBC detach histogram')
+    plt.xlabel('steps')
+    plt.ylabel('# of RBCs')
+    plt.show()
+
+    logkf = [math.log10(a) for a in koff0]
+    df = pd.DataFrame(dict(logkf=logkf, sigma=sigma, cate=cate))
+    colors = {'h': 'red', 'm': 'blue', 'l': 'green'}
+    fig2, ax = plt.subplots()
+    ax.scatter(df['logkf'],df['sigma'],c=df['cate'].apply(lambda x: colors[x]))
+    plt.show()
+
+def main():
+    plot()
+
+if __name__ == '__main__':
+    main()
